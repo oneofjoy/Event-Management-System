1b1797adb522f1c0a3e59fe871eb290bf3304fab
oneofjoy <diaszjoyandro@gmail.com>
2026-05-27 16:25:45 +0700

    Add Ticket list to DataStore for event ticket management

--- data/DataStore.java
+++ data/DataStore.java
@@ -2,12 +2,13 @@ package data;
 
 import model.User;
 import model.Event;
+import model.Ticket;
 import java.util.ArrayList;
 
 public class DataStore {
 
     public static ArrayList<User> users = new ArrayList<>();
-    
     public static ArrayList<Event> events = new ArrayList<>();
+    public static ArrayList<Ticket> tickets = new ArrayList<>();
 
 }
