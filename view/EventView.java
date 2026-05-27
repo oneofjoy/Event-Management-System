7bb62596c9a002098e4bf6d7583ee214b07b4b28
oneofjoy <diaszjoyandro@gmail.com>
2026-05-27 16:10:33 +0700

    Add EventView class for displaying event menu and notifications

---
+++ view/EventView.java
@@ -0,0 +1,17 @@
+package view;
+
+public class EventView {
+
+    public void showMenu() {
+
+        System.out.println("===== EVENT MENU =====");
+        System.out.println("1. Create Event");
+        System.out.println("2. Show Events");
+        System.out.println("3. Exit");
+    }
+
+    public void eventCreated() {
+
+        System.out.println("Event Created");
+    }
+}
