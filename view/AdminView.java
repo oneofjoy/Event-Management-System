e16bcad7186276bf5a4ef710ff38f9884bd8c712
oneofjoy <diaszjoyandro@gmail.com>
2026-05-28 10:30:30 +0700

    Add AdminView and CustomerView classes for user interface management

---
+++ view/AdminView.java
@@ -0,0 +1,12 @@
+package view;
+
+public class AdminView {
+
+    public void showMenu() {
+
+        System.out.println("\n=== ADMIN MENU ===");
+        System.out.println("1. Create Event");
+        System.out.println("2. Show Event");
+        System.out.println("3. Logout");
+    }
+}
---
+++ view/CustomerView.java
@@ -0,0 +1,13 @@
+package view;
+
+public class CustomerView {
+
+    public void showMenu() {
+
+        System.out.println("\n=== CUSTOMER MENU ===");
+
+        System.out.println("1. Buy Ticket");
+        System.out.println("2. Show Ticket");
+        System.out.println("3. Logout");
+    }
+}
